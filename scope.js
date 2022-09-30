592a13ee7ada46593241fdc3f5feb07f4b1b86ff
KishReboot <russellsalopek@pm.me>
2022-09-30 15:45:59 -0400

    Final javascripting

--- scope.js
+++ scope.js
@@ -13,7 +13,7 @@ const a = 1; const b = 2; const c = 3;
         const a = 1; const c = 8;
       })()
     })()
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
   })()
 })()
 
-console.log(`a: ${a}, b: ${b}, c: ${c}`);
